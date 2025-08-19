8b0d93f4a434ace36a9c6bf4288db360a52d9abf
Gustavo Sant'Anna <g_santanna@outlook.com>
2025-08-19 12:32:42 -0300

    fix: some improvements

--- src/App.js
+++ src/App.js
@@ -1,44 +1,37 @@
-import React, { Component } from 'react';
-import './App.css';
-import Cronometer from './cronometer';
+import React, { Component } from "react";
+import "./App.css";
+import Cronometer from "./cronometer";
+import nyanAudio from "./Nyan_Cat.mp3";
 
 class App extends Component {
   changeTimer = (owner, { target: { name } }, increase) => {
     if (increase) {
-      owner.setState((prevSt) => (
-        prevSt[name] < 59
-        ? { [name]: prevSt[name] + 1 }
-        : { [name]: 0 }
-      ));
+      owner.setState((prevSt) =>
+        prevSt[name] < 59 ? { [name]: prevSt[name] + 1 } : { [name]: 0 }
+      );
 
       owner.mouseTimeoutId = setTimeout(() => {
         owner.mouseIntervalId = setInterval(() => {
-          owner.setState((prevSt) => (
-            prevSt[name] < 59
-            ? { [name]: prevSt[name] + 1 }
-            : { [name]: 0 }
-          ));
+          owner.setState((prevSt) =>
+            prevSt[name] < 59 ? { [name]: prevSt[name] + 1 } : { [name]: 0 }
+          );
         }, 100);
       }, 800);
       return;
     }
 
-    owner.setState((prevSt) => (
-      prevSt[name] > 0
-        ? { [name]: prevSt[name] - 1 }
-        : { [name]: 59 }
-    ));
+    owner.setState((prevSt) =>
+      prevSt[name] > 0 ? { [name]: prevSt[name] - 1 } : { [name]: 59 }
+    );
 
     owner.mouseTimeoutId = setTimeout(() => {
       owner.mouseIntervalId = setInterval(() => {
-        owner.setState((prevSt) => (
-          prevSt[name] > 0
-            ? { [name]: prevSt[name] - 1 }
-            : { [name]: 59 }
-        ));
+        owner.setState((prevSt) =>
+          prevSt[name] > 0 ? { [name]: prevSt[name] - 1 } : { [name]: 59 }
+        );
       }, 100);
     }, 800);
-  }
+  };
 
   render() {
     return (
@@ -46,11 +39,12 @@ class App extends Component {
         <header className="App-header">
           <p>Nyan Catrometer</p>
         </header>
-        <Cronometer changeTimer={ this.changeTimer } />
+        <Cronometer changeTimer={this.changeTimer} />
         <div className="nyan-cat" />
         <audio
           id="nyan-audio"
-          src="https://www.albinoblacksheep.com/audio/mp3/Nyanyanyanyanyanyanya.mp3"
+          // src="https://www.albinoblacksheep.com/audio/mp3/Nyanyanyanyanyanyanya.mp3"
+          src={nyanAudio}
           preload="auto"
         />
       </div>
--- src/cronometer.jsx
+++ src/cronometer.jsx
@@ -1,10 +1,10 @@
-import React, { Component } from 'react';
-import PropTypes from 'prop-types';
-import DisplayHourCheck from './DisplayHour';
-import TimeComponent from './timeComponent';
-import TimeIsEnd from './timeIsEnd';
-import BtnComponent from './buttonComponent';
-import Infos from './infos';
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import DisplayHourCheck from "./DisplayHour";
+import TimeComponent from "./timeComponent";
+import TimeIsEnd from "./timeIsEnd";
+import BtnComponent from "./buttonComponent";
+import Infos from "./infos";
 
 class Cronometer extends Component {
   constructor() {
@@ -16,15 +16,15 @@ class Cronometer extends Component {
       hour: 0,
       minutes: 0,
       seconds: 0,
-    }
+    };
   }
 
   displayHour = ({ target: { name, checked } }) => {
     const { start } = this.state;
     if (!start) {
       this.setState({
-      [name]: checked,
-      hour: 0,
+        [name]: checked,
+        hour: 0,
       });
     }
   };
@@ -32,44 +32,49 @@ class Cronometer extends Component {
   stopEvents = () => {
     if (this.mouseTimeoutId) clearTimeout(this.mouseTimeoutId);
     if (this.mouseIntervalId) clearInterval(this.mouseIntervalId);
-  }
+  };
 
   getTotalTime() {
     const { hour, minutes, seconds } = this.state;
-    return (
-      (hour * 3600) + (minutes * 60) + seconds
-    );
+    return hour * 3600 + minutes * 60 + seconds;
   }
 
   countDown = () => {
     const { hour, minutes, seconds } = this.state;
     if (hour || minutes || seconds > 0) {
       this.setState({ start: true });
-  
-      document.querySelector('div.nyan-cat')
-        .style = `transition: ${this.getTotalTime()}s linear; margin-left: 0`;
+
+      document.querySelector(
+        "div.nyan-cat"
+      ).style = `transition: ${this.getTotalTime()}s linear; margin-left: 0`;
 
       clearInterval(this.counterId);
 
       this.counterId = setInterval(() => {
-
-        this.setState(({ hour, minutes, seconds }) => (
-          seconds > 0 ? { seconds: seconds - 1 }
-            : (minutes > 0 ? { minutes: minutes - 1, seconds: 59 }
-              : (hour > 0 ? { hour: hour - 1, minutes: 59, seconds: 59 }
-                : { start: false }))
-        ));
+        this.setState(({ hour, minutes, seconds }) =>
+          seconds > 0
+            ? { seconds: seconds - 1 }
+            : minutes > 0
+            ? { minutes: minutes - 1, seconds: 59 }
+            : hour > 0
+            ? { hour: hour - 1, minutes: 59, seconds: 59 }
+            : { start: false }
+        );
       }, 1000);
 
       const totalTime = this.getTotalTime();
-      this.endTimeId = setTimeout(() => this.timeEndAlarm(), (totalTime + 1.5) * 1000);
+      this.endTimeId = setTimeout(
+        () => this.timeEndAlarm(),
+        (totalTime + 1.5) * 1000
+      );
     }
-  }
+  };
 
   playNyanAudio() {
-    const audio = document.getElementById('nyan-audio');
-    audio.volume = 0.7;
-    audio.currentTime = 5.5;
+    const audio = document.getElementById("nyan-audio");
+    audio.volume = 1;
+    audio.currentTime = 5;
+    audio.loop = true;
     return audio;
   }
 
@@ -77,14 +82,13 @@ class Cronometer extends Component {
     clearInterval(this.counterId);
     this.setState({ end: true });
     this.playNyanAudio().play();
-  }
+  };
 
   resetCountDown = () => {
-    document.querySelector('div.nyan-cat')
-      .style = 'margin-left: -95%';
+    document.querySelector("div.nyan-cat").style = "margin-left: -95%";
     this.setState({ end: false });
     this.playNyanAudio().pause();
-  }
+  };
 
   increaseTimer = (event) => {
     const { changeTimer } = this.props;
@@ -100,30 +104,34 @@ class Cronometer extends Component {
 
   onChange = ({ target: { name, value } }) => {
     const values = value.match(/\d/g) ? value.match(/\d/g) : [0, 0];
-    const newValue= values.length > 2
-      ? +(`${values[values.length - 2]}${values[values.length - 1]}`) : value;
-    const entry = +(newValue) > 59 ? 59 : newValue;
+    const newValue =
+      values.length > 2
+        ? +`${values[values.length - 2]}${values[values.length - 1]}`
+        : value;
+    const entry = +newValue > 59 ? 59 : newValue;
     this.setState((prevSt) => ({
-      [name]: isNaN(+(entry)) ? +(prevSt[name]) : +(entry),
+      [name]: isNaN(+entry) ? +prevSt[name] : +entry,
     }));
   };
 
   pauseCounter = () => {
     clearInterval(this.counterId);
     clearTimeout(this.endTimeId);
-    document.querySelector('div.nyan-cat')
-      .style = `transition: ${this.getTotalTime() / 2}s linear; margin-left: -95% `;
+    document.querySelector("div.nyan-cat").style = `transition: ${
+      this.getTotalTime() / 2
+    }s linear; margin-left: -95% `;
+
     this.setState({
       start: false,
     });
     this.playNyanAudio().pause();
-  }
+  };
 
   resetCounter = () => {
     clearInterval(this.counterId);
     clearTimeout(this.endTimeId);
-    document.querySelector('div.nyan-cat')
-      .style = 'transition: 1s linear; margin-left: -95%';
+    document.querySelector("div.nyan-cat").style =
+      "transition: 1s linear; margin-left: -95%";
     this.setState({
       end: false,
       start: false,
@@ -132,56 +140,79 @@ class Cronometer extends Component {
       seconds: 0,
     });
     this.playNyanAudio().pause();
-  }
+  };
 
   render() {
-    const { end, start,  viewHour, hour, minutes, seconds } = this.state;
+    const { end, start, viewHour, hour, minutes, seconds } = this.state;
+
     return (
       <>
-      <Infos />
-      <section className="cronometer-box">
-        <DisplayHourCheck curCondition={ viewHour } getCondition={ this.displayHour } />
-        <div className="cronometer">
-          { viewHour && <><TimeComponent
-            unit="hour"
-            curTime={ +(hour) }
-            ifStart={ start }
-            increase={ this.increaseTimer }
-            decrease={ this.decreaseTimer }
-            clearEv={ this.stopEvents }
-            change={ this.onChange }
-          /> : </> }
-          <TimeComponent
-            unit="minutes"
-            curTime={ +(minutes) }
-            ifStart={ start }
-            increase={ this.increaseTimer }
-            decrease={ this.decreaseTimer }
-            clearEv={ this.stopEvents }
-            change={ this.onChange }
-          /> :
-          <TimeComponent
-            unit="seconds"
-            curTime={ +(seconds) }
-            ifStart={ start }
-            increase={ this.increaseTimer }
-            decrease={ this.decreaseTimer }
-            clearEv={ this.stopEvents }
-            change={ this.onChange }
+        <Infos />
+        <section className="cronometer-box">
+          <DisplayHourCheck
+            curCondition={viewHour}
+            getCondition={this.displayHour}
           />
-        </div>
-        { !start 
-          ? <BtnComponent buttonClass="start-btn" countFunc={ this.countDown } content="Start" />
-          : <BtnComponent buttonClass="pause-btn" countFunc={ this.pauseCounter } content="Pause" />
-        }
-        <BtnComponent buttonClass="reset-btn" countFunc={ this.resetCounter } content="RESET" />
-          { end && <TimeIsEnd timeEnd={ this.resetCountDown } /> }
-      </section>
+          <div className="cronometer">
+            {viewHour && (
+              <>
+                <TimeComponent
+                  unit="hour"
+                  curTime={+hour}
+                  ifStart={start}
+                  increase={this.increaseTimer}
+                  decrease={this.decreaseTimer}
+                  clearEv={this.stopEvents}
+                  change={this.onChange}
+                />{" "}
+                :{" "}
+              </>
+            )}
+            <TimeComponent
+              unit="minutes"
+              curTime={+minutes}
+              ifStart={start}
+              increase={this.increaseTimer}
+              decrease={this.decreaseTimer}
+              clearEv={this.stopEvents}
+              change={this.onChange}
+            />{" "}
+            :
+            <TimeComponent
+              unit="seconds"
+              curTime={+seconds}
+              ifStart={start}
+              increase={this.increaseTimer}
+              decrease={this.decreaseTimer}
+              clearEv={this.stopEvents}
+              change={this.onChange}
+            />
+          </div>
+          {!start ? (
+            <BtnComponent
+              buttonClass="start-btn"
+              countFunc={this.countDown}
+              content="Start"
+            />
+          ) : (
+            <BtnComponent
+              buttonClass="pause-btn"
+              countFunc={this.pauseCounter}
+              content="Pause"
+            />
+          )}
+          <BtnComponent
+            buttonClass="reset-btn"
+            countFunc={this.resetCounter}
+            content="RESET"
+          />
+          {end && <TimeIsEnd timeEnd={this.resetCountDown} />}
+        </section>
       </>
     );
   }
 }
 
-Cronometer.propTypes = { changeTimer: PropTypes.func.isRequired }
+Cronometer.propTypes = { changeTimer: PropTypes.func.isRequired };
 
 export default Cronometer;
